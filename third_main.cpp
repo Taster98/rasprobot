bc0b94743ff7f4604ea397d8e8608e19e9096bf8
Taster98 <luigi.gesuele98@gmail.com>
2020-11-20 22:07:56 +0100

    3rd commit | Trial to use keyboard events to handle movements

--- third_main.cpp
+++ third_main.cpp
@@ -96,11 +96,5 @@ void logicPart(){
         status=system("sudo killall python3");
         //avvio il comando avanti
         status = system("python3 movements/right.py&");
-    }else if(!right && !left && !up && !down){
-        //SE NESSUN TASTO È PREMUTO ALLORA FERMO TUTTO
-        //pausa gli altri comandi
-        status=system("python3 movements/pause.py");
-        //uccido gli altri comandi
-        status=system("sudo killall python3");
     }
 }
